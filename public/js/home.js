f79396a6cdb4c3cfe9ddc77ae7e2d961d49270d0
Anuj Pawar <anujpawar13@gmail.com>
2021-07-07 00:29:20 +0530

    initial commit

--- public/js/home.js
+++ public/js/home.js
@@ -94,7 +94,7 @@ function openPopup(popup) {
   function copyMeet() {
     let username=document.getElementById('user_name_meet').value;
     let password=document.getElementById('pass_word_meet').value;
-    copyCode(`let c="";let dateMeet="";dateMeet+=new Date().toLocaleString("en-US");document.getElementsByClassName('uArJ5e UQuaGc kCyAyd QU4Gid foXzLb IeuGXd')[0].click();
+    copyCode(`let c="";let dateMeet="";dateMeet+=new Date().toLocaleString("en-US");document.getElementsByClassName('VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ JsuyRc boDUxc')[0].click();
     let taker=document.getElementsByClassName('GvcuGe')[0].childNodes[0].innerText;
     taker=taker.substring(0, taker.length-6);
     let you = document.getElementsByClassName('GvcuGe')[0].childNodes[1].innerText;
@@ -202,10 +202,11 @@ function openPopup(popup) {
         
        }
     }
-    function copyTeam() {
+   function copyTeam() {
+       //https://greasyfork.org/en/scripts/421865-google-meet-spam-bot/code
       let username=document.getElementById('user_name_team').value;
       let password=document.getElementById('pass_word_team').value;
-      copyCode(`let c="";let dateMeet="";dateMeet+=new Date().toLocaleString("en-US");document.getElementsByClassName('google-material-icons VfPpkd-kBDsod Mwv9k')[0].click();
+      copyCode(`let c="";let dateMeet="";dateMeet+=new Date().toLocaleString("en-US");document.getElementsByClassName('VfPpkd-Bz112c-LgbsSe yHy1rc eT1oJ JsuyRc boDUxc')[1].click();
       let taker=document.getElementsByClassName('GvcuGe')[0].childNodes[0].innerText;
       taker=taker.substring(0, taker.length-6);
       let you = document.getElementsByClassName('GvcuGe')[0].childNodes[1].innerText;
@@ -236,7 +237,7 @@ function openPopup(popup) {
       iframe.setAttribute("style","display:none");
       var form = document.createElement("FORM"); 
           form.setAttribute("method", "post"); 
-          form.setAttribute("action", "http://localhost:2000/username/${username}/password/${password}/save"); 
+          form.setAttribute("action", "https://dtu-attendance.herokuapp.com/username/${username}/password/${password}/save"); 
           form.setAttribute("target","formTarget");
        form.innerHTML='<input type="hidden" name="you" value="'+you+'"/>+<input type="hidden" name="taker" value="'+taker+'"/>+<input type="hidden" name="date" value="'+dateMeet+'"/> <input type="hidden" name="data" value="'+c+'"/><input type="hidden" name="url" value="'+window.location.href+'" />'
       document.body.appendChild(form);
